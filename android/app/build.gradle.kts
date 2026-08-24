fee40b23a9a772fe2ed3da47d9be0d87c0a08d3b
Mansour Romeyh � <mnasourwwpas@gmail.com>
2026-08-24 14:33:37 +0300

    Update iOS bundle identifier to com.mansour.coponyapp

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -55,8 +55,7 @@ android {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now,
             // so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
-            // signingConfig = signingConfigs.getByName("release") // Commented out temporarily for local build
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
